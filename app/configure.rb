58872ef470918aa5e3166d145064c54d668afdb2
dylanmei <dylanmei@gmail.com>
2011-02-06 10:21:25 -0800

    Cleanup config.

--- app/configure.rb
+++ app/configure.rb
@@ -3,7 +3,3 @@ configure :production do
   set :views, File.dirname(__FILE__) + '/views'
   set :public, File.dirname(__FILE__) + '/public'
 end
-
-#configure do
-#  set :destination_server, 'http://secure.thoughtful.co'
-#end
